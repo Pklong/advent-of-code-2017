838af0088f10535498aae0da25019343431c4a7f
PKLong <patrickklong@gmail.com>
2017-12-23 11:06:02 -0500

    finish day 13

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 - [x] Day 10
 - [x] Day 11
 - [x] Day 12
-- [ ] Day 13
+- [x] Day 13
 - [ ] Day 14
 - [ ] Day 15
 - [ ] Day 16
--- day_13.js
+++ day_13.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 
-fs.readFile('./test.txt', 'utf-8', (err, data) => {
+fs.readFile('./input_13.txt', 'utf-8', (err, data) => {
   if (err) throw err
+
   let lastLayer = 0
   const result = data
     .split('\n')
@@ -13,21 +14,70 @@ fs.readFile('./test.txt', 'utf-8', (err, data) => {
       acc[el[0]] = el[1]
       return acc
     }, {})
-  const fireWall = new Array(lastLayer).fill(undefined).map((e, i) => {
-    if (result[i]) {
-      const layer = [i, new Array(result[i]).fill(false)]
-      layer[1][0] = true
-      return layer
-    } else {
-      return [i, []]
+
+  const fireWall = new Array(lastLayer + 1)
+    .fill(undefined)
+    .map((e, i) => {
+      if (result[i]) {
+        return Layer(result[i])
+      } else {
+        return Layer(0)
+      }
+    })
+
+  let totalSeverity = 0
+
+  for (let i = 0; i < fireWall.length; i++) {
+    const layer = fireWall[i]
+    if (layer.length === 0) continue
+    const time = (delay + i) % (layer.length * 2 - 2)
+    if (time === 0) {
+      totalSeverity += i * layer.length
     }
-  })
-  console.log(fireWall)
+  }
+
+  let delay = 0
+  let caught = true
+
+  while (caught) {
+    caught = false
+    for (let i = 0; i < fireWall.length; i++) {
+      const layer = fireWall[i]
+      if (layer.length === 0) continue
+      const time = (delay + i) % (layer.length * 2 - 2)
+      if (time === 0) {
+        delay++
+        caught = true
+        break
+      }
+    }
+  }
+  console.log(
+    'if starting immediately, your caught level is',
+    totalSeverity
+  )
+  console.log('To avoid getting caught, start on picosecond ', delay)
 })
 
-function scan(pointer, arr, dir) {}
-
-// position of pointer
-// direction of pointer
-// check out of bounds
-// increment / decrement
+// wrote this to simulate moving through firewall
+// before steps % length * 2 - 2 insight...
+function Layer(length) {
+  return {
+    dir: 1,
+    pointer: 0,
+    length,
+    scan: function() {
+      if (
+        this.pointer + this.dir >= this.length ||
+        this.pointer + this.dir < 0
+      ) {
+        this.dir = this.dir > 0 ? -1 : 1
+      }
+      this.pointer += this.dir
+    },
+    reset: function() {
+      this.pointer = 0
+      this.dir = 1
+    }
+  }
+}
